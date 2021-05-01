2c160e1a2fbf2510b818dae4acf28893c45b1b1a
Guilherme <guiborgesilva@outlook.com>
2021-04-30 21:29:26 -0300

    teste de fonte

--- pages/_document.js
+++ pages/_document.js
@@ -12,6 +12,7 @@ export default class MyDocument extends Document{
           <link rel="favicon" href="icon.png" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
+          <link rel="preload" href="../public/fonts/goodvibrations-script.ttf" as="font" />
         </Head>
         <body>
           <Main />
